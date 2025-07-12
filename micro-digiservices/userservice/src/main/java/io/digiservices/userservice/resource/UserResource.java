a259472375a97fa0c677b6314f12cdc5bbccf6b8
doukoure93 <douklifsa93@gmail.com>
2025-07-12 10:04:40 +0000

    Update Migration file

--- micro-digiservices/userservice/src/main/java/io/digiservices/userservice/resource/UserResource.java
+++ micro-digiservices/userservice/src/main/java/io/digiservices/userservice/resource/UserResource.java
@@ -443,9 +443,5 @@ public class UserResource {
         return ResponseEntity.ok("This is offline test Migration test2");
     }
 
-    @GetMapping("/offLine/test3")
-    public ResponseEntity<String> offLineTest3()
-    {
-        return ResponseEntity.ok("This is offline test Migration with test3 and so on");
-    }
+
 }
