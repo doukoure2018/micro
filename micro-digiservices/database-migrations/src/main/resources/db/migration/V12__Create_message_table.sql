93aba7fa110661b768258f157dbf5831713768fb
doukoure93 <douklifsa93@gmail.com>
2025-07-13 23:02:25 +0000

    Update Sql migration

--- micro-digiservices/database-migrations/src/main/resources/db/migration/V12__Create_message_table.sql
+++ micro-digiservices/database-migrations/src/main/resources/db/migration/V12__Create_message_table.sql
@@ -2,5 +2,5 @@ CREATE TABLE IF NOT EXISTS messages (
     message_id BIGSERIAL PRIMARY KEY,
     subject VARCHAR(100) NOT NULL,
     message TEXT NOT NULL,
-    sender_id BIGINT NOT NULL,
+    sender_id BIGINT NOT NULL
     );
