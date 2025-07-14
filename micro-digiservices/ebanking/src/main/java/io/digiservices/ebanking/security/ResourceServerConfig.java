7d00c47b6bd8ceeee3075cc1bd8f7ac21df4175d
doukoure93 <douklifsa93@gmail.com>
2025-07-14 10:19:20 +0000

    UPDATE FRONTEND

--- micro-digiservices/ebanking/src/main/java/io/digiservices/ebanking/security/ResourceServerConfig.java
+++ micro-digiservices/ebanking/src/main/java/io/digiservices/ebanking/security/ResourceServerConfig.java
@@ -35,7 +35,7 @@ public class ResourceServerConfig {
         http.csrf(AbstractHttpConfigurer::disable)
                 .cors(cors -> cors.configurationSource(corsConfigurationSource()))
                 .authorizeHttpRequests( authorize -> authorize
-                        .requestMatchers("/user/register/**", "/user/verify/account/**", "/user/resetpassword/**", "/user/image/**", "/authorization/**","/ebanking/search/**","/ebanking/typeCredit/**","/ebanking/agences/**","/ebanking/pointventes/**","/ebanking/pointvente/**","/ebanking/agence/**","/ebanking/clientes/**","/ebanking/typePlazo/**","/ebanking/nombreCreditos/**","/ebanking/cumulCredito/**","ebanking/inversion/**","/ebanking/activida/**","/ebanking/getInfoActivida/**","/ebanking/sousActivite/**",
+                        .requestMatchers("/actuator/health","/actuator/info","/user/register/**", "/user/verify/account/**", "/user/resetpassword/**", "/user/image/**", "/authorization/**","/ebanking/search/**","/ebanking/typeCredit/**","/ebanking/agences/**","/ebanking/pointventes/**","/ebanking/pointvente/**","/ebanking/agence/**","/ebanking/clientes/**","/ebanking/typePlazo/**","/ebanking/nombreCreditos/**","/ebanking/cumulCredito/**","ebanking/inversion/**","/ebanking/activida/**","/ebanking/getInfoActivida/**","/ebanking/sousActivite/**",
                                 "/ebanking/sousSousActivite/**","/ebanking/requisitos/**","/ebanking/origineFond/**","/ebanking/typeCredito/**","/ebanking/create/**","/ebanking/getUsuarios/**","/ebanking/creditos/**","/ebanking/planPagos/**","/ebanking/search/**","/ebanking/offLine/**").permitAll()
                         .anyRequest().authenticated())
                 .oauth2ResourceServer(oauth2 -> oauth2
