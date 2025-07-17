67fc701d9251489f00344d6c3c71eefead065539
doukoure93 <douklifsa93@gmail.com>
2025-07-17 08:11:33 +0000

    Update Configuration to prod

---
+++ frontend/ultima-ng-19.0.0/src/app/interface/template.modification.ts
@@ -0,0 +1,9 @@
+interface TemplateModification {
+    id: number;
+    nom: string;
+    description: string;
+    categorie: string;
+    motifDefaut: string;
+    elementsJson: any;
+    nombreUtilisations: number;
+}
